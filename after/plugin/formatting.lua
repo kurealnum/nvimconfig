86b7f1e9a2179508a3847210bd67c085358f4a00
kurealnum <cattledoger@gmail.com>
2024-02-05 16:10:40 -0500

    Formatters added. STABLE

--- after/plugin/formatting.lua
+++ after/plugin/formatting.lua
@@ -1,25 +1,45 @@
 require("formatter").setup({
-  logging = true,
-  filetype = {
-    css = {
-      require("formatter.filetypes.css").prettier,
-    },
-	python = {
-		require("formatter.filetypes.python").black,
+	logging = true,
+	filetype = {
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		css = {
+			require("formatter.filetypes.css").prettier,
+		},
+		python = {
+			require("formatter.filetypes.python").black,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
+		json = {
+			require("formatter.filetypes.json").prettier,
+		},
+		markdown = {
+			require("formatter.filetypes.markdown").prettier,
+		},
+		vue = {
+			require("formatter.filetypes.vue").prettier,
+		},
+		html = {
+			require("formatter.filetypes.html").prettier,
+		},
+		htmldjango = {
+			function()
+				return {
+					exe = "djlint",
+					args = {
+						"--reformat",
+						"--format-css",
+						"--format-js",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
 	},
-    javascript = {
-      require("formatter.filetypes.javascript").prettier,
-    },
-    json = {
-      require("formatter.filetypes.json").prettier,
-    },
-    markdown = {
-      require("formatter.filetypes.markdown").prettier,
-    },
-    vue = {
-      require("formatter.filetypes.vue").prettier,
-    },
-  },
 })
 
 local augroup = vim.api.nvim_create_augroup
@@ -29,4 +49,3 @@ autocmd("BufWritePost", {
 	group = "__formatter__",
 	command = ":FormatWrite",
 })
-
