3fb35c24ceccd64dca59a290bdc4814d06923714
kurealnum <cattledoger@gmail.com>
2024-02-08 23:07:31 -0500

    Wallpapers added, this might be a general config soon. STABLE

--- lua/kureal/packer.lua
+++ lua/kureal/packer.lua
@@ -1,89 +1,96 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
 		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-	use { 'catppuccin/nvim', as = 'catppuccin' }
-	use 'iagorrr/noctis-high-contrast.nvim'
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("iagorrr/noctis-high-contrast.nvim")
 
-	use 'nvim-treesitter/nvim-treesitter'
+	use("nvim-treesitter/nvim-treesitter")
 
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		requires = {
 			--- Uncomment the two plugins below if you want to manage the language servers from neovim
 			--- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
 			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
+			{ "neovim/nvim-lspconfig" },
 			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'L3MON4D3/LuaSnip' },
-		}
-	}
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
 
-	use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-		require("toggleterm").setup()
-	end }
-
-	use "sindrets/diffview.nvim"
-
-	use {
-		'rmagatti/auto-session',
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
 		config = function()
-			require("auto-session").setup {
+			require("toggleterm").setup()
+		end,
+	})
+
+	use("sindrets/diffview.nvim")
+
+	use({
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
 				log_level = "error",
 				auto_session_suppress_dirs = { "~/", "/Code", "~/Downloads", "/" },
-			}
-		end
-	}
+			})
+		end,
+	})
 
-	use 'nvim-tree/nvim-web-devicons'
+	use("nvim-tree/nvim-web-devicons")
 
-	use {
-		'numToStr/Comment.nvim',
+	use({
+		"numToStr/Comment.nvim",
 		config = function()
-			require('Comment').setup()
-		end
-	}
+			require("Comment").setup()
+		end,
+	})
 
-	use "mhartington/formatter.nvim"
+	use("mhartington/formatter.nvim")
 
-	use 'lewis6991/gitsigns.nvim'
+	use("lewis6991/gitsigns.nvim")
 
-	use {
+	use({
 		"nvim-telescope/telescope-file-browser.nvim",
-		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-	}
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	})
 
-	use {
+	use({
 		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-	}
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
 
-	use {
+	use({
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
@@ -91,19 +98,18 @@ return require('packer').startup(function(use)
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		},
-	}
+	})
 
-	use {
-		'matthewmazzanti/nvim-ts-autotag',
-		config = function ()
-			require('nvim-ts-autotag').setup({
-				filetypes = {"html", "xml", "htmldjango"}
+	use({
+		"matthewmazzanti/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				filetypes = { "html", "xml", "htmldjango" },
 			})
-		end
-	}
+		end,
+	})
 
-	use {
-		'lukas-reineke/indent-blankline.nvim',
-	}
-
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+	})
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -122,7 +122,6 @@ _G.packer_plugins = {
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["indent-blankline.nvim"] = {
-    config = { "\27LJ\2\n1\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\bibl\frequire\0" },
     loaded = true,
     path = "/home/oscar/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
@@ -228,6 +227,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 -- Config for: auto-session
 time([[Config for auto-session]], true)
 try_loadstring("\27LJ\2\nè\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\31auto_session_suppress_dirs\1\5\0\0\a~/\n/Code\16~/Downloads\6/\1\0\1\14log_level\nerror\nsetup\17auto-session\frequire\0", "config", "auto-session")
@@ -240,18 +243,10 @@ time([[Config for nvim-ts-autotag]], false)
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
--- Config for: toggleterm.nvim
-time([[Config for toggleterm.nvim]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
-time([[Config for toggleterm.nvim]], false)
 -- Config for: nvim-autopairs
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
--- Config for: indent-blankline.nvim
-time([[Config for indent-blankline.nvim]], true)
-try_loadstring("\27LJ\2\n1\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\bibl\frequire\0", "config", "indent-blankline.nvim")
-time([[Config for indent-blankline.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
