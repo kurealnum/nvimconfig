b45c5cc91adf3d4456781c1bb7d15d77206e9167
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-10 22:52:02 -0400

    Made a bunch of changes to colors. STABLE

--- after/plugin/transparencyandcolors.lua
+++ after/plugin/transparencyandcolors.lua
@@ -1,12 +1,17 @@
-local transparency = require("transparent")
+-- enable mason and popup transparency (not sure why I have to do this)
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none", fg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none", fg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none", fg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none", fg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none", fg = "none" })
+	end,
+})
 -- colorscheme
-vim.cmd("colorscheme earlysummer_lighter")
--- enable lualine transparency
-transparency.clear_prefix("lualine")
+vim.cmd("colorscheme rose-pine-moon")
 -- enable global transparency
 vim.cmd("TransparentEnable")
--- enable mason and popup transparency
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
--- lua/kureal/init.lua
+++ lua/kureal/init.lua
@@ -3,9 +3,14 @@ require("kureal.set")
 
 vim.wo.relativenumber = true
 
-vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#b4befe" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#777777" })
 
 vim.cmd("command Q qa!")
 
 vim.g.python3_host_prog = "/usr/bin/python3"
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- round border
+vim.diagnostic.config({
+	float = { border = "rounded" },
+})
