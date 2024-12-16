45d2d98e6a0751885de14b5754d01662bf50f858
kurealnum <oscar.gaske.cs@gmail.com>
2024-12-15 19:54:32 -0500

    Changed file path

--- after/plugin/image.lua
+++ after/plugin/image.lua
@@ -6,7 +6,7 @@ image.setup({
 			floating_windows = false,
 			resolve_image_path = function(document_path, image_path, fallback)
 				local working_dir = vim.fn.getcwd()
-				if working_dir:find("/media/1TB/DualBootFiles/Obsidian Notes") then
+				if working_dir:find("/home/oscar/obsidian-notes/") then
 					return working_dir .. "/Images/" .. image_path
 				end
 				return fallback(document_path, image_path)
