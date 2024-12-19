4211a0eccaee5a0514337086644a943a921a8850
kurealnum <oscar.gaske.cs@gmail.com>
2024-12-19 15:56:46 -0500

    Changed markdown plugins. STABLE

--- lua/kureal/lazy.lua
+++ lua/kureal/lazy.lua
@@ -153,13 +153,14 @@ require("lazy").setup({
 			dependencies = { "nvim-lua/plenary.nvim", "folke/trouble.nvim" },
 		},
 		{
-			"MeanderingProgrammer/render-markdown.nvim",
-			dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-			-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-			---@module 'render-markdown'
-			---@type render.md.UserConfig
-			opts = {},
+			"OXY2DEV/markview.nvim",
+			lazy = false, -- Recommended
+			-- ft = "markdown" -- If you decide to lazy-load anyway
+
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+				"nvim-tree/nvim-web-devicons",
+			},
 		},
 		{
 			"lervag/vimtex",
