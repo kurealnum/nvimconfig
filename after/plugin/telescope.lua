ff408615aa59d3f16107d178b095085254f73d10
kurealnum <oscar.gaske.cs@gmail.com>
2025-08-25 21:19:17 -0400

    lsp stuff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,9 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- open symbols
+vim.keymap.set("n", "<leader>pl", builtin.lsp_document_symbols, {})
